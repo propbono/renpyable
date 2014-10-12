ba3dcc3f8f19fb4615f280a9bda4a6950c74257e
ProPBono <propbono@users.noreply.github.com>
2014-10-12 15:10:22 +0200

    Add 2 classes to config.py Class RealConfig with production configuration values and class FakeConfig with values strictly for testing purposes

--- config.py
+++ config.py
@@ -1,10 +1,26 @@
 # Configuration file
+class RealConfig:
+	# Constant to control version of programm
+	VERSION = "0.1.1"
 
-# Constant to control version of programm
-VERSION = "0.1.1"
+	# Constant to determine where TIFF files are stored
+	TIF_DIRECTORY = 'Q:\\TIFF\\Test\\'
+	PPF_DIRECTORY = 'B:\\'
+	PDF_ARCHIVE_DIRECTORY = 'F:\\00Archiwum - PDFdoCIP\\'
+	DOA_DIRECTORY = 'W:\\'
 
-# Constant to determine where TIFF files are stored
-TIF_DIRECTORY = 'Q:\\TIFF\\Test\\'
-PPF_DIRECTORY = 'B:\\'
-PDF_ARCHIVE_DIRECTORY = 'F:\\00Archiwum - PDFdoCIP\\'
-DOA_DIRECTORY = 'W:\\'
+class FakeConfig:
+	# Constant to control version of programm
+	VERSION = "0.2.4"
+
+	# Constant to determine where TIFF files are stored
+	TIF_DIRECTORY = '/home/propbono/Projects/Python/RenPyAble/tif/'	# check and change to this form os.path.dirname(sys.argv[0])
+	PPF_DIRECTORY = '/home/propbono/Projects/Python/RenPyAble/ppf/'	# for compatibility with windows
+	PDF_ARCHIVE_DIRECTORY = '/home/propbono/Projects/Python/RenPyAble/pdf/'
+	DOA_DIRECTORY = '/home/propbono/Projects/Python/RenPyAble/doa/'
+	
+	'''
+	def getScriptPath():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+	'''
+
