fe3471b2a7dbf84c0399d84185f67d38c451ecc5
propbono <propbono@gmail.com>
2014-11-07 09:36:10 +0100

    New skeleton method correct_pdf_filenames(digit) was added
    Improve this method like is written in documentation

--- RenPyAble.py
+++ RenPyAble.py
@@ -117,7 +117,7 @@ class Renpyable:
 			print("		Change: Yes")
 			print("		New Name: ", new_name, "\n")
 			print("	----------------------------------------------------\n")
-	
+
 
 	def _correct_names(self):
 		"""
@@ -173,6 +173,23 @@ class Renpyable:
 	def get_modification_time(self, filename):
 		time = os.path.getmtime(filename)
 		return datetime.date.fromtimestamp(time)
+
+	def correct_pdf_filenames(self, digits):
+		"""
+			Implement listening option, and hotfloders
+			You copy files to hotfolder i.e. pdf_in 
+			The are processed there and after finished they are copied to pdf_out
+			
+			Corects pdf chunk files from clients,
+			and preparing them for binding. <-- bad word
+		    i.e. 1_time.pdf, 20_movies.pdf 
+		    will change to: 001_time.pdf and 020_movies.pdf
+		"""
+		for name in pdf_file_list:
+		if name.endswith('pdf') or name.endswith("PDF"):
+			number = re.search(r'\d+', name).group()
+			new_name = name.replace(number, number.zfill(3))
+			os.rename(name,new_name)
 
 if __name__ == "__main__":
 	# global variable for checking while loop
