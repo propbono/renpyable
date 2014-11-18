55976f9bded164401d6b0e7f33d150beb72a4103
Grzegorz Mozer <propbono@gmail.com>
2014-11-18 23:03:06 +0100

    All tests to method correct_tiff_filenames are pases

--- rename.py
+++ rename.py
@@ -1,9 +1,20 @@
+import re
+
 class Rename:
-    def correct_tiff_filenames(filename):
-        """ Function changes upper case to lower case,\n"
-            changes also word front to afront\n"
+    def correct_tiff_filenames(filename, number_of_digits):
+        """ Function changes upper case to lower case,
+            changes also word front to afront
+            changes also number of digits in numbers
         """
         filename = filename.lower()
         if filename.endswith("tif") or filename.endswith('tiff'):
             filename = filename.replace("front", "afront")
-            return filename
+
+            numbers_to_replace = re.findall(r'\d+',filename)
+            if numbers_to_replace:
+                unique_numbers_to_replace = set(numbers_to_replace)
+                for number in unique_numbers_to_replace:
+                    filename = filename.replace(number,
+                                        number.zfill(number_of_digits))
+
+        return filename
--- test_rename.py
+++ test_rename.py
@@ -4,31 +4,33 @@ from rename import Rename
 
 
 class RenameTests(unittest.TestCase):
-    def test_correct_tiff_filenames_method_should_affect_files_with_tif_extension(
-            self):
+    def test_correct_tiff_filenames_method_should_affect_files_with_tif_extension(self):
         filename = "test_file.tif"
-        result = Rename.correct_tiff_filenames(filename)
+        number_of_digits = 2
+        result = Rename.correct_tiff_filenames(filename, number_of_digits)
         expected = "tif"
-        self.assertTrue(result,
-                        msg="Expected extension: " + expected + " Actual extension: " + self.extension(
-                            result))
+        self.assertTrue(result, msg="Expected extension: " + expected
+                                + " Actual extension: " + self.extension( result))
 
     def test_correct_tiff_filenames_method_should_affect_files_with_tiff_extension(self):
         filename = "test_file.tiff"
-        result = Rename.correct_tiff_filenames(filename)
+        number_of_digits = 2
+        result = Rename.correct_tiff_filenames(filename, number_of_digits)
         expected = "tiff"
         self.assertTrue(result,msg="Expected extension: " + expected + " Actual extension: " + self.extension(result))
 
     def test_correct_tiff_filenames_method_should_affect_also_files_with_capital_letters(self):
         filename = "test_file.TIF"
-        result = Rename.correct_tiff_filenames(filename)
+        number_of_digits = 2
+        result = Rename.correct_tiff_filenames(filename, number_of_digits)
         expected = "tif"
         self.assertEqual(expected, self.extension(result),
                          msg="Expected extension: " + expected + " Actual extension: " + self.extension(
                              result))
 
         filename = "test_file.TIFF"
-        result = Rename.correct_tiff_filenames(filename)
+        number_of_digits = 2
+        result = Rename.correct_tiff_filenames(filename, number_of_digits)
         expected = "tiff"
         self.assertEqual(expected, self.extension(result),
                          msg="Expected extension: " + expected + " Actual extension: " + self.extension(
@@ -36,23 +38,37 @@ class RenameTests(unittest.TestCase):
 
     def test_correct_tiff_filenames_method_should_change_word_FRONT_to_AFRONT(self):
         filename = "Test_File_FOLD1_FRONT.tif"
-        result = Rename.correct_tiff_filenames(filename)
-        expected = "test_file_fold1_afront.tif"
-        self.assertEqual(result,expected, "The two filenames are not equal")
-
+        number_of_digits = 2
+        result = Rename.correct_tiff_filenames(filename, number_of_digits)
+        expected = "test_file_fold01_afront.tif"
+        self.assertEqual(expected, result, "The two filenames are not equal")
 
     def test_correct_tiff_filenames_method_should_change_number_1_to_01(self):
-        self.assertTrue(False,"NotImplemented")
+        filename = "Test_File_FOLD1_FRONT.tif"
+        number_of_digits = 2
+        result = Rename.correct_tiff_filenames(filename,number_of_digits)
+        expected = "test_file_fold01_afront.tif"
+        self.assertEqual(expected,result, "The two filenames are not equal")
 
-    def test_correct_tiff_filenames_method_should_change_number_10_to_010(self):
-        self.assertTrue(False,"NotImplemented")
+    def test_correct_tiff_filenames_method_should_change_number_2_to_02(self):
+        filename = "Test_File_FOLD2_FRONT.tif"
+        number_of_digits = 2
+        result = Rename.correct_tiff_filenames(filename,number_of_digits)
+        expected = "test_file_fold02_afront.tif"
+        self.assertEqual(expected,result, "The two filenames are not equal")
 
-    def test_correct_tiff_filenames_method_should_change_number_100_to_0100(self):
-        self.assertTrue(False,"NotImplemented")
+    def test_correct_tiff_filenames_method_should_change_all_numbers_in_filename(self):
+        filename = "Test_File_N1_FOLD1_BACK.tif"
+        number_of_digits = 3
+        result = Rename.correct_tiff_filenames(filename, number_of_digits)
+        expected = "test_file_n001_fold001_back.tif"
+        self.assertEqual(expected,result, "The two filenames are not equal")
 
-    def test_correct_tiff_filenames_method_should_change_number_02_to_0002(self):
-        self.assertTrue(False,"NotImplemented")
-
+        filename = "Test_File_N2_FOLD1_BACK.tif"
+        number_of_digits = 3
+        result = Rename.correct_tiff_filenames(filename, number_of_digits)
+        expected = "test_file_n002_fold001_back.tif"
+        self.assertEqual(expected,result, "The two filenames are not equal")
 
     def extension(self, filename):
         return filename.split('.')[1]
