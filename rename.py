f9a27bd8efa63bcc1ed5e0a262c59dba28bd99f8
Grzegorz Mozer <propbono@gmail.com>
2014-11-18 23:20:08 +0100

    Change name of correct_tiff_files method to correct_filename. Change to static method

--- rename.py
+++ rename.py
@@ -1,20 +1,24 @@
 import re
 
 class Rename:
-    def correct_tiff_filenames(filename, number_of_digits):
+    @staticmethod
+    def correct_filename(filename, number_of_digits):
         """ Function changes upper case to lower case,
-            changes also word front to afront
-            changes also number of digits in numbers
-        """
+                changes also word front to afront
+                changes also number of digits in numbers i.e. 1 = 001
+
+                :param filename:
+                :param number_of_digits:
+                :rtype : string
+            """
         filename = filename.lower()
         if filename.endswith("tif") or filename.endswith('tiff'):
             filename = filename.replace("front", "afront")
 
-            numbers_to_replace = re.findall(r'\d+',filename)
+            numbers_to_replace = re.findall(r'\d+', filename)
             if numbers_to_replace:
                 unique_numbers_to_replace = set(numbers_to_replace)
                 for number in unique_numbers_to_replace:
                     filename = filename.replace(number,
-                                        number.zfill(number_of_digits))
-
+                                                number.zfill(number_of_digits))
         return filename
