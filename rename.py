a088d8c3639dc0019bb905ca448e634e3e668f3f
Grzegorz Mozer <propbono@gmail.com>
2014-11-18 21:18:58 +0100

    Add passing test to change word front to afront in tiff file name

--- rename.py
+++ rename.py
@@ -1,7 +1,9 @@
 class Rename:
-
-    def rename_tiff_files(filename):
+    def correct_tiff_filenames(filename):
+        """ Function changes upper case to lower case,\n"
+            changes also word front to afront\n"
+        """
         filename = filename.lower()
         if filename.endswith("tif") or filename.endswith('tiff'):
+            filename = filename.replace("front", "afront")
             return filename
-
--- test_rename.py
+++ test_rename.py
@@ -1,53 +1,66 @@
 import unittest
+
 from rename import Rename
 
+
 class RenameTests(unittest.TestCase):
-
-    def test_rename_tiff_files_method_should_affect_files_with_tif_extension(self):
+    def test_correct_tiff_filenames_method_should_affect_files_with_tif_extension(
+            self):
         filename = "test_file.tif"
-        result = Rename.rename_tiff_files(filename)
+        result = Rename.correct_tiff_filenames(filename)
         expected = "tif"
-        self.assertTrue(result, msg="Expected extension: "+expected+" Actual extension: "+ self.extension(result))
+        self.assertTrue(result,
+                        msg="Expected extension: " + expected + " Actual extension: " + self.extension(
+                            result))
 
-    def test_rename_tiff_files_method_should_affect_files_with_tiff_extension(self):
+    def test_correct_tiff_filenames_method_should_affect_files_with_tiff_extension(self):
         filename = "test_file.tiff"
-        result = Rename.rename_tiff_files(filename)
+        result = Rename.correct_tiff_filenames(filename)
         expected = "tiff"
-        self.assertTrue(result,  msg="Expected extension: "+expected+" Actual extension: "+ self.extension(result))
+        self.assertTrue(result,msg="Expected extension: " + expected + " Actual extension: " + self.extension(result))
 
-    def test_rename_tiff_files_method_should_affect_also_files_with_capital_letters(self):
+    def test_correct_tiff_filenames_method_should_affect_also_files_with_capital_letters(self):
         filename = "test_file.TIF"
-        result = Rename.rename_tiff_files(filename)
+        result = Rename.correct_tiff_filenames(filename)
         expected = "tif"
-        self.assertEqual(expected, self.extension(result),  msg="Expected extension: "+expected+" Actual extension: "+ self.extension(result))
+        self.assertEqual(expected, self.extension(result),
+                         msg="Expected extension: " + expected + " Actual extension: " + self.extension(
+                             result))
 
         filename = "test_file.TIFF"
-        result = Rename.rename_tiff_files(filename)
+        result = Rename.correct_tiff_filenames(filename)
         expected = "tiff"
-        self.assertEqual(expected, self.extension(result),  msg="Expected extension: "+expected+" Actual extension: "+ self.extension(result))
+        self.assertEqual(expected, self.extension(result),
+                         msg="Expected extension: " + expected + " Actual extension: " + self.extension(
+                             result))
 
-    def test_rename_tiff_files_method_should_change_word_FRONT_to_AFRONT(self):
-        unittest.skip("NotImplemented")
-
-    def test_rename_tiff_files_method_should_change_number_1_to_01(self):
-        unittest.skip("NotImplemented")
-
-    def test_rename_tiff_files_method_should_change_number_10_to_010(self):
-        unittest.skip("NotImplemented")
-
-    def test_rename_tiff_files_method_should_change_number_100_to_0100(self):
-        unittest.skip("NotImplemented")
-
-    def test_rename_tiff_files_method_should_change_number_02_to_0002(self):
-        unittest.skip("NotImplemented")
+    def test_correct_tiff_filenames_method_should_change_word_FRONT_to_AFRONT(self):
+        filename = "Test_File_FOLD1_FRONT.tif"
+        result = Rename.correct_tiff_filenames(filename)
+        expected = "test_file_fold1_afront.tif"
+        self.assertEqual(result,expected, "The two filenames are not equal")
 
 
-    def extension(self,filename):
-       return filename.split('.')[1]
+    def test_correct_tiff_filenames_method_should_change_number_1_to_01(self):
+        self.assertTrue(False,"NotImplemented")
+
+    def test_correct_tiff_filenames_method_should_change_number_10_to_010(self):
+        self.assertTrue(False,"NotImplemented")
+
+    def test_correct_tiff_filenames_method_should_change_number_100_to_0100(self):
+        self.assertTrue(False,"NotImplemented")
+
+    def test_correct_tiff_filenames_method_should_change_number_02_to_0002(self):
+        self.assertTrue(False,"NotImplemented")
+
+
+    def extension(self, filename):
+        return filename.split('.')[1]
 
 
 def main():
     unittest.main()
+
 
 if __name__ == '__main__':
     main()
